80d54eb096e789e0fecaafddd3f433cdcac762c5
= <=>
2021-09-25 09:04:18 +0100

    refactor

--- update-home-dns.py
+++ update-home-dns.py
@@ -1,12 +1,33 @@
 from datetime import datetime
-from re import M
 import CloudFlare
 import ipaddress
-from CloudFlare import cloudflare
 import requests
+import os
 
-def get_host_zone_name(host_name: str) -> str:
-    return host_name.split(".", 1)[1]
+def main():
+    host_name = get_env_variable('DNS_HOST_NAME', 'Desired DNS hostname must be supplied in an environment variable called DNS_HOST_NAME')
+    cf_token = get_env_variable('CF_TOKEN', 'CloudFlare token must be supplied in an environment variable called CF_TOKEN')
+    cf = CloudFlare.CloudFlare(cf_token)
+    current_ip = get_public_ip()
+    cf_dns_zone_id = get_cf_dns_zone_id(cf, host_name)
+    cf_dns_record = get_cf_dns_record(cf, cf_dns_zone_id, host_name)
+    update_cf_if_required(cf, cf_dns_zone_id, cf_dns_record, current_ip)
+
+def get_env_variable(var_name: str, error_msg: str):
+    value = os.getenv(var_name)
+    if (value == None):
+        exit(error_msg)
+    return value
+
+def get_public_ip() -> ipaddress.IPv4Address:
+    url = 'http://ifconfig.me/ip'
+    address = requests.get(url).text
+    log(f'Current public IP address is {address}')
+    return ipaddress.ip_address(address)
+
+def log(message: str):
+    now = datetime.now()
+    print(f'{now} - {message}')
 
 def get_cf_dns_zone_id(cf: CloudFlare.CloudFlare, host_name: str) -> str:
     try:
@@ -19,7 +40,10 @@ def get_cf_dns_zone_id(cf: CloudFlare.CloudFlare, host_name: str) -> str:
     except Exception as e:
         exit(f'Unable to retrieve DNS zone {zone_name} from CloudFlare API: {e}')
 
-def get_cf_dns_record(cf: CloudFlare, zone_id: str, host_name: str) -> ipaddress.IPv4Address:
+def get_host_zone_name(host_name: str) -> str:
+    return host_name.split(".", 1)[1]
+
+def get_cf_dns_record(cf: CloudFlare.CloudFlare, zone_id: str, host_name: str) -> ipaddress.IPv4Address:
     try:
         records = cf.zones.dns_records.get(zone_id, params={'name':host_name})
         if (len(records) != 1):
@@ -29,13 +53,20 @@ def get_cf_dns_record(cf: CloudFlare, zone_id: str, host_name: str) -> ipaddress
     except Exception as e:
         exit(f'Unable to retrieve DNS record {host_name} from CloudFlare API: {e}')
 
-def get_public_ip() -> ipaddress.IPv4Address:
-    url = 'http://ifconfig.me/ip'
-    address = requests.get(url).text
-    log(f'Current public IP address is {address}')
-    return ipaddress.ip_address(address)
+def get_ip_from_cf_dns(dns_record: object) -> ipaddress.IPv4Address:
+    ip = ipaddress.ip_address(dns_record['content'])
+    dns_name = dns_record['name']
+    log(f'CloudFlare DNS record {dns_name} currently has value {ip}')
+    return ip
 
-def update_dns_record(cf: CloudFlare, zone_id: str, old_dns_record: object, new_ip: ipaddress.IPv4Address):
+def update_cf_if_required(cf: CloudFlare.CloudFlare, zone_id: str, dns_record: object, current_ip: ipaddress.IPv4Address):
+    current_dns_ip = get_ip_from_cf_dns(dns_record)
+    if (current_ip != current_ip):
+        update_dns_record(cf, zone_id, dns_record, current_ip)
+    else:
+        log('No update required')
+
+def update_dns_record(cf: CloudFlare.CloudFlare, zone_id: str, old_dns_record: object, new_ip: ipaddress.IPv4Address):
     try:
         host_name = old_dns_record['name']
         log(f'Updating DNS record {host_name} to {new_ip} via the CloudFlare API')
@@ -48,28 +79,6 @@ def update_dns_record(cf: CloudFlare, zone_id: str, old_dns_record: object, new_
         new_dns_record = cf.zones.dns_records.put(zone_id, old_dns_record['id'], data=new_dns_record)
     except Exception as e:
         exit(f'Unable to update the DNS record {host_name} to {new_ip} via the CloudFlare API: {e}')
-
-def log(message: str):
-    now = datetime.now()
-    print(f'{now} - {message}')
-
-
-def main():
-    host_name = 'ENTER HOSTNAME'
-    cf = CloudFlare.CloudFlare(token='ENTER TOKEN')
-    current_ip = get_public_ip()
-    cf_dns_zone_id = get_cf_dns_zone_id(cf, host_name)
-    cf_dns_record = get_cf_dns_record(cf, cf_dns_zone_id, host_name)
-    cf_dns_record_ip = ipaddress.ip_address(cf_dns_record['content'])
-    log(f'Current value of DNS record {host_name} is {cf_dns_record_ip}')
-    
-    if (current_ip != cf_dns_record_ip):
-        update_dns_record(cf, cf_dns_zone_id, cf_dns_record, current_ip)
-    else:
-        print("No update required")
-
-    
-    
 
 if __name__ == '__main__':
     main()
